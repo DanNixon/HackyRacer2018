5f68c4b99c2a79bb9af56832625314ccd2aea517
Dan Nixon <dan@dan-nixon.com>
2019-11-16 18:43:53 +0000

    Motor

---
+++ frame/parts/motor.py
@@ -0,0 +1,72 @@
+import solid as sp
+import solid.utils as spu
+
+from frame.utils import place_at_centres
+
+body_diameter = 108
+body_length = 140
+
+mounting_hole_centres = 90
+mounting_hole_diameter = 5
+
+shaft_diameter = 12
+shaft_length = 30
+
+sprocket_diameter = 30
+sprocket_pos = 16
+
+shaft_surround_diam = 22
+
+vent_holes_centres = 65
+vent_holes_diameter = 10
+
+
+def motor():
+    mounting_holes = spu.down(1)(
+        place_at_centres(
+            [0, mounting_hole_centres],
+            sp.cylinder(d=mounting_hole_diameter, h=body_length + 2)
+        )
+    )
+
+    body = sp.color('orange')(
+        sp.rotate(
+            [0, 180, 0]
+        )(sp.cylinder(d=body_diameter, h=body_length) - mounting_holes, )
+    )
+
+    shaft = sp.color('black')(sp.cylinder(d=shaft_diameter, h=shaft_length), )
+
+    sprocket = sp.color('cyan')(
+        spu.up(sprocket_pos)
+        (sp.cylinder(d=sprocket_diameter, h=5, center=True), )
+    )
+
+    return sp.union()(
+        body,
+        shaft,
+        sprocket,
+    )
+
+
+def mountable_face():
+    outer = sp.circle(d=body_diameter)
+
+    shaft_cutout = sp.circle(d=shaft_surround_diam)
+
+    mounting_holes = place_at_centres(
+        [0, mounting_hole_centres], sp.circle(d=mounting_hole_diameter)
+    )
+
+    _vent_hole = spu.forward(vent_holes_centres / 2.
+                             )(sp.circle(d=vent_holes_diameter))
+    vent_holes = [
+        sp.rotate([0, 0, a * 360. / 8.])(_vent_hole) for a in range(8)
+    ]
+
+    return outer - shaft_cutout - mounting_holes - vent_holes
+
+
+if __name__ == '__main__':
+    # print(sp.scad_render(motor()))
+    print(sp.scad_render(mountable_face()))
