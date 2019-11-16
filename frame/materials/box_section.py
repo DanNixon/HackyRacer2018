0531615402a51f4420de8a361c47800e2467f48a
Dan Nixon <dan@dan-nixon.com>
2019-11-16 17:42:19 +0000

    Materials WIP

---
+++ frame/materials/box_section.py
@@ -0,0 +1,28 @@
+import solid as sp
+import solid.utils as spu
+
+
+def projection(size, wall_thickness, center):
+    outer = sp.square(size, center=center)
+    inner = sp.square([a - 2 * wall_thickness for a in size], center=center)
+    outer -= inner if center else sp.translate(
+        [wall_thickness, wall_thickness]
+    )(inner)
+    return outer
+
+
+def box_section(
+        length,
+        size=[25, 25],
+        wall_thickness=2,
+        center=False,
+        material='mild_steel'
+):
+    material = sp.linear_extrude(length)(
+        projection(size, wall_thickness, center)
+    )
+    return spu.down(length / 2)(material) if center else material
+
+
+if __name__ == '__main__':
+    print(sp.scad_render(box_section(100, size=[10, 20])))
---
+++ frame/materials/tube.py
@@ -0,0 +1,20 @@
+import solid as sp
+import solid.utils as spu
+
+
+def projection(diameter, wall_thickness):
+    outer = sp.circle(d=diameter)
+    inner = sp.circle(d=diameter - 2 * wall_thickness)
+    return outer - inner
+
+
+def tube(
+        length, diameter=25, wall_thickness=2, center=False,
+        material='mild_steel'
+):
+    material = sp.linear_extrude(length)(projection(diameter, wall_thickness))
+    return spu.down(length / 2)(material) if center else material
+
+
+if __name__ == '__main__':
+    print(sp.scad_render(tube(100)))
