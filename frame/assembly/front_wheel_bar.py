14fc3aa7bdee9a304e7a0a301035b2ab83b84eaf
Dan Nixon <dan@dan-nixon.com>
2019-11-18 18:53:36 +0000

    Convert wheel bar

--- frame/assembly/front_wheel_bar.py
+++ frame/assembly/front_wheel_bar.py
@@ -2,6 +2,7 @@ import solid as sp
 import solid.utils as spu
 
 from frame.assembly import wheel_centre_distance
+from frame.materials import box_section
 
 bar_thickness = 5
 
@@ -10,41 +11,39 @@ bar_length = wheel_centre_distance - (100 * 2) - 70
 inner_height = 35
 inner_depth = 40
 
-# rotate([0, 0, 90])
-# {
-#   BoxSection(
-#       name="lower_frame/front_wheel_x_bar",
-#       col="red",
-#       outer=box_section_outer,
-#       length=bar_length,
-#       center=true);
-# }
-
-# color("green")
-# {
-#   for(a = [0, 180])
-#   {
-#     rotate([0, 0, a])
-#     {
-#       translate([(bar_length + bar_thickness) / 2, 0, 0])
-#       {
-#         cube([bar_thickness, box_section_outer[0], inner_height + (2 * bar_thickness)], center=true);
-
-#         for (z = [-inner_height/2 - bar_thickness, inner_height/2])
-#         {
-#           translate([0, -box_section_outer[0] / 2, z])
-#           {
-#             cube([inner_depth + bar_thickness, box_section_outer[0], bar_thickness]);
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
 
 def assembly():
-    return sp.union()()
+    bar = sp.rotate([0, 90, 0])(box_section.volume(bar_length, center=True))
+
+    # TODO: design this in 2D and extrude
+    _mount = spu.right((bar_length + bar_thickness) / 2.)(
+        sp.union()(
+            sp.cube(
+                [
+                    bar_thickness, box_section.default_size[0], inner_height +
+                    (2. * bar_thickness)
+                ],
+                center=True
+            ),
+            [
+                sp.translate([0, -box_section.default_size[0] / 2., d])(
+                    sp.cube(
+                        [
+                            inner_depth + bar_thickness,
+                            box_section.default_size[0], bar_thickness
+                        ]
+                    )
+                )
+                for d in [-inner_height / 2 - bar_thickness, inner_height / 2.]
+            ],
+        )
+    )
+    _mounts = [sp.rotate([0, 0, a])(_mount) for a in [0, 180]]
+
+    return sp.union()(
+        sp.color(spu.Red)(bar),
+        sp.color(spu.Green)(_mounts),
+    )
 
 
 if __name__ == '__main__':
