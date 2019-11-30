040b5db71acff51faa767c95b4f67f1d283685f5
Dan Nixon <dan@dan-nixon.com>
2019-11-30 13:18:48 +0000

    WIP: adjust for new wheels

--- frame/parts/wheel.py
+++ frame/parts/wheel.py
@@ -2,32 +2,32 @@ import solid as sp
 
 from frame.utils import bom
 
+outer_diameter = 260
+thickness = 90
 
 @bom.part('Wheel')
 def volume():
     big = 250
-    thickness = 100
 
-    outer = sp.cylinder(d=290, h=thickness, center=True)
+    outer = sp.cylinder(d=outer_diameter, h=thickness, center=True)
 
     wheel_cutouts = [
         sp.rotate([0, a, 0])(
             sp.translate([0, 0, -thickness / 2 - 0.1])(
-                sp.cylinder(d=180, h=15),
-                sp.cylinder(d=130, h=45),
+                sp.cylinder(d=120, h=40),
             )
         ) for a in [0, 180]
     ]
 
     bolts = [
-        sp.rotate([0, 0, a * 360 / 5])(
-            sp.translate([0, 50, 0])(sp.cylinder(d=8, h=big, center=True))
-        ) for a in range(5)
+        sp.rotate([0, 0, a * 360 / 4])(
+            sp.translate([0, 45, 0])(sp.cylinder(d=8, h=big, center=True))
+        ) for a in range(4)
     ]
 
     shaft = sp.cylinder(d=16, h=big, center=True),
 
-    bearing_mount = sp.cylinder(h=thickness / 2, d=38)
+    bearing_mount = sp.cylinder(h=thickness / 2, d=40)
 
     return sp.color("darkgray")(
         sp.rotate([0, 90, 0])(
