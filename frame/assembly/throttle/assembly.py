1981a16feb8981e26c33d2f828bc4c1002f7b138
Dan Nixon <dan@dan-nixon.com>
2019-12-26 19:25:31 +0000

    WIP: new throttle mount design

--- frame/assembly/throttle/assembly.py
+++ frame/assembly/throttle/assembly.py
@@ -1,14 +1,19 @@
 import solid as sp
+import solid.utils as spu
 
 from frame.utils import entrypoint
 
-from . import mount, lever
+from . import lever, mount
 
 
 def assembly():
     return sp.union()(
-        sp.color('red')(mount.volume()),
-        sp.color('green')(lever.volume()),
+        sp.color('red')(
+            # TODO
+            spu.down(20)
+            (sp.rotate((0, 90, 0))(sp.rotate((0, 0, -90))(lever.volume())))
+        ),
+        sp.color('green')(mount.volume()),
     )
 
 
--- frame/assembly/throttle/dimensions.py
+++ frame/assembly/throttle/dimensions.py
@@ -1,9 +1,6 @@
 lever_thickness = 5.
-layer_spacing = 6.
-
-mount_thickness = 3.
 
 pot_shaft_diameter = 6.
 pot_mount_diameter = 7.5
 
-mount_screw_diameter = 4.5
+mount_lever_spacing = 6.
