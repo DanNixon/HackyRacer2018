5337127c5a8e3fd5157e9f516db1137d11a80659
Dan Nixon <dan@dan-nixon.com>
2019-12-27 17:20:25 +0000

    Add steering column to assembly

--- frame/assembly/assembly.py
+++ frame/assembly/assembly.py
@@ -10,7 +10,7 @@ from frame.materials import box_section
 from frame.utils import entrypoint
 
 from .dimensions import inner, inner_length, wheel_centre_distance, rear_axle_position, outer_length, front_bumper_depth
-from . import bumpers, motor, rear_axle
+from . import bumpers, motor, rear_axle, steering
 
 
 def assembly():
@@ -18,44 +18,50 @@ def assembly():
 
     return sp.union()(
         sp.color('red')(lower_frame.assembly()),
-        sp.color('green')(sp.translate([0, 180, 160])(seat_mount.assembly())),
+        sp.color('green')(sp.translate((0, 180, 160))(seat_mount.assembly())),
         sp.color('blue')(
             sp.translate(
-                [
+                (
                     0, rear_axle_position, -(box_section.default_size[0] / 2.) -
                     rear_axle_bearing.shaft_height
-                ]
+                )
             )(rear_axle.assembly())
         ),
         sp.color('cyan')(
             sp.translate(
-                [0, inner_length + box_section.default_size[0] / 2., 0]
+                (0, inner_length + box_section.default_size[0] / 2., 0)
             )(
                 [
-                    sp.rotate([0, 0, a])(
+                    sp.rotate((0, 0, a))(
                         spu.right(wheel_centre_distance / 2. - 100)(
                             front_wheel.assembly()
                         )
-                    ) for a in [0, 180]
+                    ) for a in (0, 180)
                 ]
             )
         ),
         sp.color('magenta')(
-            sp.translate([inner + box_section.default_size[0] / 2., 300,
-                          0])(sp.rotate([90, 0, -90])(motor.assembly()))
+            sp.translate((inner + box_section.default_size[0] / 2., 300, 0))(
+                sp.rotate((90, 0, -90))(motor.assembly())
+            )
         ),
         sp.color('pink')(
             spu.forward(
                 inner_length + front_bumper_depth + box_section.default_size[0]
-            )(sp.rotate([90, 0, 180])(bumpers.front.assembly())),
+            )(sp.rotate((90, 0, 180))(bumpers.front.assembly())),
             spu.back(box_section.default_size[1])(
-                sp.rotate([90, 0, 0])(bumpers.rear.assembly())
+                sp.rotate((90, 0, 0))(bumpers.rear.assembly())
             ),
         ),
         sp.color('lime')(
             sp.translate(
-                [-inner + (box_section.default_size[0] / 2.) + 1., magic_1, 0]
+                (-inner + (box_section.default_size[0] / 2.) + 1., magic_1, 0)
             )(brake_pedal.assembly())
+        ),
+        sp.color('orange')(
+            sp.translate((0, 950, 0))(
+                sp.rotate((45, 0, 0))(steering.assembly())
+            )
         ),
     )
 
