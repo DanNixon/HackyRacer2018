adf9fbaf1641784cd832475d0d883bfceb1ea92b
Dan Nixon <dan@dan-nixon.com>
2019-12-29 16:23:18 +0000

    Motor mount dimensions
    May also possibly be 12mm.

--- frame/assembly/motor/mount.py
+++ frame/assembly/motor/mount.py
@@ -5,25 +5,25 @@ from frame.utils import entrypoint, place_at_centres
 
 from . import motor
 
-plate_thickness = 6
+plate_thickness = 10.
 
-mount_holes_offset = 70
-mount_holes_distance = 80
-mount_holes_diameter = 6
+mount_holes_offset = 70.
+mount_holes_distance = 80.
+mount_holes_diameter = 6.
 
-lower_extent = mount_holes_offset + 15
+lower_extent = mount_holes_offset + 15.
 
 
 def projection():
-    d = motor.body_diameter + 10
+    d = motor.body_diameter + 10.
     outer = sp.hull()(
-        sp.square([d, d], center=True),
-        spu.back(lower_extent - 5)(sp.square([d, 10], center=True)),
+        sp.square((d, d), center=True),
+        spu.back(lower_extent - 5.)(sp.square((d, 10.), center=True)),
     )
 
     mounting_holes = spu.back(mount_holes_offset)(
         place_at_centres(
-            [mount_holes_distance, 0], sp.circle(d=mount_holes_diameter)
+            (mount_holes_distance, 0.), sp.circle(d=mount_holes_diameter)
         )
     )
 
