99738cf0a06ae1deda0f6965e8fdcdfade620099
Dan Nixon <dan@dan-nixon.com>
2020-01-20 18:02:31 +0000

    Motor mount slots

--- frame/assembly/motor/mount.py
+++ frame/assembly/motor/mount.py
@@ -9,10 +9,18 @@ plate_width = 120.
 plate_thickness = 10.
 
 mount_holes_offset = 70.
-mount_holes_distance = 80.
-mount_holes_diameter = 6.
+mount_holes_distance = 60.
+mount_slots_size = (40., 6.)
 
 lower_extent = mount_holes_offset + 15.
+
+
+def mounting_slot():
+    return sp.hull()(
+        place_at_centres(
+            (mount_slots_size[0] / 2., 0.), sp.circle(d=mount_slots_size[1])
+        )
+    )
 
 
 def projection():
@@ -22,11 +30,8 @@ def projection():
         spu.back(lower_extent - 5.)(sp.square((plate_width, 10.), center=True)),
     )
 
-    # TODO: slots
     mounting_holes = spu.back(mount_holes_offset)(
-        place_at_centres(
-            (mount_holes_distance, 0.), sp.circle(d=mount_holes_diameter)
-        )
+        place_at_centres((mount_holes_distance, 0.), mounting_slot())
     )
 
     return outer - motor.mountable_face() - mounting_holes
