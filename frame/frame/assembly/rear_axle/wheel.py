039f3ea704321df03ce23136ba89d1760a367d34
Dan Nixon <dan@dan-nixon.com>
2020-01-23 16:40:34 +0000

    Update rear wheel CAD

--- frame/frame/assembly/rear_axle/wheel.py
+++ frame/frame/assembly/rear_axle/wheel.py
@@ -1,43 +1,48 @@
 import solid as sp
 import solid.utils as spu
 
-from frame.utils import bom, entrypoint
+from frame.utils import bom, entrypoint, place_n_at_x_around
 
 outer_diameter = 260
 thickness = 80
 bolt_hole_diameter = 8.
+inner_hole_diameter = 32.
+
+
+def mounting_holes():
+    bolt = place_n_at_x_around(
+        4, 35.,
+        sp.cylinder(d=bolt_hole_diameter, h=thickness + 1., center=True)
+    )
+
+    inner = sp.cylinder(d=inner_hole_diameter, h=thickness + 1., center=True),
+
+    return sp.union()(
+        bolt,
+        inner,
+    )
 
 
 @bom.part('Rear Wheel')
 def volume():
     outer = sp.cylinder(d=outer_diameter, h=thickness, center=True)
 
-    wheel_cutouts = [
+    magic_1 = (thickness - 5.) / 2.
+    hub = [
         sp.rotate((0, a, 0))(
             spu.down((thickness / 2.) + 0.1)(
-                sp.cylinder(d=120, h=15),
-                sp.cylinder(d=90, h=40),
+                sp.cylinder(d=85., h=magic_1),
+                sp.cylinder(d=120., h=15.),
             )
         ) for a in [0, 180]
     ]
 
-    bolts = [
-        sp.rotate([0, 0, a * (360. / 4.)])(
-            spu.forward(35.)
-            (sp.cylinder(d=bolt_hole_diameter, h=thickness + 1., center=True))
-        ) for a in range(4)
-    ]
-
-    # TODO: measure hole diameter
-    shaft = sp.cylinder(d=30., h=thickness + 1., center=True),
-
     return sp.color("darkgray")(
-        sp.rotate((0, 90, 0))(
-            sp.difference()(
-                sp.union()(outer - wheel_cutouts - bolts, ),
-                shaft,
-            )
-        )
+        sp.rotate((0, 90, 0))(sp.difference()(
+            outer,
+            hub,
+            mounting_holes(),
+        ))
     )
 
 
