ce15e5420688e2ab710f1d8ec3428f48f621e4ad
Dan Nixon <dan@dan-nixon.com>
2020-01-30 06:33:23 +0000

    Refactor steering arm

--- cad/frame/assembly/steering/arm.py
+++ cad/frame/assembly/steering/arm.py
@@ -1,7 +1,7 @@
 import solid as sp
 import solid.utils as spu
 
-from frame.materials import flat_bar
+from frame.materials import plate
 from frame.primitives import drilled_hole
 from frame.utils import entrypoint, place_at_centres, place_n_at_x_around
 
@@ -14,7 +14,7 @@ mounting_hole_offset = 20.
 
 tie_rod_hole_diameter = 5.
 tie_rod_hole_centres = 35.
-tie_rod_hole_offset = length - 6.
+tie_rod_hole_offset = length - 8.
 
 
 def place_mounting_holes(obj):
@@ -24,32 +24,29 @@ def place_mounting_holes(obj):
 
 
 def projection():
-    # TODO
-    pass
-
-
-def volume():
     from . import arm_mount
-    magic_1 = (arm_mount.face_diameter / 2.) - 5.
+    magic_1 = (arm_mount.face_diameter / 2.)
 
-    bar = sp.translate((0., magic_1, thickness / 2.))(
-        sp.rotate((90, 0, 0))(
-            flat_bar.volume(length=length + magic_1, size=(width, thickness))
-        )
+    bar = sp.translate((0., magic_1, 0.))(
+        plate.projection(size=(width, length + magic_1))
     )
 
     mounting_holes = place_mounting_holes(
-        drilled_hole.volume(mounting_hole_diameter)
+        drilled_hole.projection(mounting_hole_diameter)
     )
 
-    tie_rod_holes = spu.back(tie_rod_hole_offset)(
+    tie_rod_holes = spu.forward(tie_rod_hole_offset)(
         place_at_centres(
             (tie_rod_hole_centres, 0.),
-            drilled_hole.volume(tie_rod_hole_diameter)
+            drilled_hole.projection(tie_rod_hole_diameter)
         )
     )
 
     return bar - mounting_holes - tie_rod_holes
+
+
+def volume():
+    return sp.linear_extrude(thickness)(projection())
 
 
 if __name__ == '__main__':
