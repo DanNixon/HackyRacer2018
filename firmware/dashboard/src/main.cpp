6f85c23bba20f3038c149710cb96c4606f36252e
Dan Nixon <dan@dan-nixon.com>
2020-02-02 10:13:19 +0000

    wip

--- firmware/dashboard/src/main.cpp
+++ firmware/dashboard/src/main.cpp
@@ -2,25 +2,18 @@
 #include <SD.h>
 #undef swap
 
-#include "dashboard/value/value.hpp"
-
-using namespace dashboard;
+#include "value/value.hpp"
 
 /* SPI */
 /* MOSI 11 */
 /* MISO 12 */
 /* SCLK 13 */
-constexpr auto tft_cs(10);
-constexpr auto tft_dc(9);
-constexpr auto tft_reset(8);
 ILI9341_t3 tft(tft_cs, tft_dc, tft_reset);
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   /* while (!Serial) */
-  {
-  }
+  {}
   Serial.println("Hello");
 
   /* SD card */
@@ -38,7 +31,6 @@ void setup()
   tft.print("test");
 }
 
-void loop(void)
-{
+void loop(void) {
   /* TODO */
 }
---
+++ firmware/dashboard/src/pins.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace pins {
+constexpr auto tft_cs = 10;
+constexpr auto tft_dc = 9;
+constexpr auto tft_reset = 8;
+} // namespace pins
